e5dc21d887dea192fa137e4566a632c8980260ae
chrisgbradley <chris@chrisgbradley>
2021-12-02 13:12:55 -0500

    fix css/html uniformity

--- source/dashboard/dash_apps/tuition.py
+++ source/dashboard/dash_apps/tuition.py
@@ -8,24 +8,28 @@ import plotly.graph_objs as go  # pip install dash (version 2.0.0 or higher)
 from django_plotly_dash import DjangoDash
 
 
-app = DjangoDash("Tuition")
+app = DjangoDash(name="Tuition")
 
 
 # -- Import our data into the code as a panda data frame.
-df = pd.read_csv("https://raw.githubusercontent.com/chrisgbradley/itsc-3155-final-group-2/main/AdmissionsData.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/chrisgbradley/itsc-3155-final-group-2/main/data/Tuition-CSV.csv")
 
 # -- Our data is technically already "clean" due to how we formatted it in excel (thanks Christian).
 
 # -- Needed variables for page formatting.
 
 # Not to be confused with YEAR from our data CSV
-YEARS = [1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
+YEARS = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
 
 
 # -- Test print, if needed.
 # print(df[:40])
 
 # -------------------------------------------------------------------------------------------------------------------
+
+plotly_layout = go.Layout(
+    paper_bgcolor='rgb(81, 81, 89, 1)', plot_bgcolor='rgb(54, 54, 64, 1)'
+)
 
 # -- App Layout
 app.layout = html.Div([
@@ -35,54 +39,46 @@ app.layout = html.Div([
 
     html.H1("Test In State Tuition Graph", style={'text-align': 'center'}),
 
-#   html.Div(
-#        id="dropdown-container",
-#        children=[
-#            html.P(
-#                id="dropdown-text",
-#               children="Select whether you want In State or Out of State Tution",
-#            ),
-#            dcc.Dropdown(
-#                id="in-or-out",
-#                options=[
-#                    {'label' : 'In State Tuition', 'value' : "TUITION_IN"},
-#                    {'label' : 'Out of State Tuition', 'value' : "TUITION_OUT"},
-#                ],
-#                value='TUITION_IN',
-#                multi=False
-#            ),
-#        ],
-#    ),
-
     html.Div(
         id="slider-container",
         children=[
-                html.P(
-                    id="slider-text",
-                    children="Drag the slider to change the year:",
-                ),
-                dcc.Slider(
-                    id="years-slider",
-                    min=min(YEARS),
-                    max=max(YEARS),
-                    value=min(YEARS),
-                    marks={
-                        str(year): {
-                            "label": str(year),
-                            "style": {"color": "#7fafdf"},
-                        }
-                        for year in YEARS
-                    },
-                ),
+            html.P(
+                id="slider-text",
+                children="Drag the slider to change the year:",
+            ),
+            dcc.Slider(
+                id="years",
+                min=min(YEARS),
+                max=max(YEARS),
+                value=min(YEARS),
+                marks={
+                    str(year): {
+                        "label": str(year),
+                        "style": {
+                            "color": "#009688",
+                            # "font-size": "20px",
+                            # "font-family": "Roboto, Sans-Serif"
+                        },
+                    }
+                    for year in YEARS
+                },
+                included=False
+            ),
         ],
     ),
 
-    html.Div(id='output_container', children=[]),
+    html.Div(id='tabs', children=[
+        # dcc.Graph(id='in_tuition_map', figure={}),
+        # dcc.Graph(id='out_tuition_map', figure={})
+        dcc.Tabs(id="in-out-tabs", value="in-state", children=[
+            dcc.Tab(label="In-State Tuition", value="in-state"),
+            dcc.Tab(label="Out-Of-State Tuition", value="out-state")
+        ])
+    ]),
 
     html.Br(),
 
-    dcc.Graph(id='in_tuition_map', figure={}),
-    dcc.Graph(id='out_tuition_map', figure={})
+    html.Div(id="tab-graph-output")
 ])
 
 # ------------------------------------------------------------------------------
@@ -96,58 +92,80 @@ app.layout = html.Div([
 # bee map is for containing our plotly graph. The input is the users selected year. This will be changeable through the
 # dropdown component.
 
-@app.callback(
-    [Output(component_id='output_container', component_property='children'),
-     Output(component_id='in_tuition_map', component_property='figure')],
-    [Input(component_id='years-slider', component_property='value')]
-)
 
-def update_in_graph(option_slctd):
-    print(option_slctd)
-    print(type(option_slctd))
+def change(layout):
+    # print(layout)
+    # print(layout.template)
+    modified_layout = layout
+    modified_layout.height = 720
+    modified_layout.width = 1320
+    modified_layout.plot_bgcolor = 'rgb(54, 54, 64)'
+    modified_layout.paper_bgcolor = 'rgb(81, 81, 89)'
+    modified_layout.font.color = 'rgb(255, 255, 255)'
+    modified_layout.coloraxis.colorscale = [
+        [0.0,   '#ffffff'],
+        [0.125, '#ccf8ee'],
+        [0.25,  '#a9ecdf'],
+        [0.375, '#8ddfd0'],
+        [0.5,   '#73d1c1'],
+        [0.625, '#5bc2b2'],
+        [0.75,  '#43b4a4'],
+        [0.875, '#2aa596'],
+        [1.0,   '#009688'],
+    ]
+    modified_layout.geo.bgcolor = 'rgb(54, 54, 64)'
+    modified_layout.geo.lakecolor = 'rgb(54, 54, 64)'
 
-    container = "The year chosen by user was: {}".format(option_slctd)
+    return modified_layout
 
+
+@app.callback(Output('tab-graph-output', 'children'),
+              [Input('in-out-tabs', 'value'),
+               Input('years', 'value')])
+def render_content(tab, year):
     dff = df.copy()
-    dff = dff[dff["YEAR"] == option_slctd]
+    dff = dff[dff["YEAR"] == year]
 
-    fig = px.choropleth(
-        data_frame=dff,
-        locationmode='USA-states',
-        locations='STATE',
-        scope="usa",
-        color='TUITION_IN',
-        hover_data=['STATE', 'TUITION_IN'],
-        color_continuous_scale=px.colors.sequential.YlOrRd,
-        labels={'TUITION_IN': 'Average In State Tuition'},
-        template='plotly_dark'
-    )
+    if tab == 'in-state':
 
-    return container, fig
+        fig_in = px.choropleth(
+            data_frame=dff,
+            locationmode='USA-states',
+            locations='STATE',
+            scope="usa",
+            color='TUITION_IN',
+            hover_data=['STATE', 'TUITION_IN'],
+            color_continuous_scale=px.colors.sequential.YlOrRd,
+            labels={'TUITION_IN': 'Avg In-State Tuition'},
+        )
 
-@app.callback(
-    [Output(component_id='out_tuition_map', component_property='figure')],
-    [Input(component_id='years-slider', component_property='value')]
-)
+        fig_in.layout = change(fig_in.layout)
 
-def update_out_graph(YEARS):
+        return html.Div([
+            html.H3('In-State Tuition'),
+            dcc.Graph(id='in-state-graph',
+                      figure=fig_in)
+        ])
+    elif tab == 'out-state':
 
-    dff_out = df.copy()
-    dff_out = dff_out[dff_out["YEAR"] == YEARS]
+        fig_out = px.choropleth(
+            data_frame=dff,
+            locationmode='USA-states',
+            locations='STATE',
+            scope="usa",
+            color='TUITION_OUT',
+            hover_data=['STATE', 'TUITION_OUT'],
+            color_continuous_scale=px.colors.sequential.YlOrRd,
+            labels={'TUITION_OUT': 'Avg Out-of-State Tuition'},
+        )
 
-    fig = px.choropleth(
-        data_frame=dff_out,
-        locationmode='USA-states',
-        locations='STATE',
-        scope="usa",
-        color='TUITION_OUT',
-        hover_data=['STATE', 'TUITION_OUT'],
-        color_continuous_scale=px.colors.sequential.YlOrRd,
-        labels={'TUITION_OUT': 'Average Out of State Tuition'},
-        template='plotly_dark'
-    )
+        fig_out.layout = change(fig_out.layout)
 
-    return fig
+        return html.Div([
+            html.H3('In-State Tuition'),
+            dcc.Graph(id='in-state-graph',
+                      figure=fig_out)
+        ])
 
 
 # ------------------------------------------------------------------------------
